5abaf3427957e77e5f3ddbd435b1dbe30a91a357
Xavier Paredes <jxavierpp1@gmail.com>
2022-07-29 19:43:44 -0700

    ✨ Add mock data

---
+++ src/data/foodItems.js
@@ -0,0 +1,52 @@
+const FOOD_TYPES = [
+    {
+        id: 0,
+        name: 'Verduras',
+        actual: 0,
+        meta: 3
+    },
+    {
+        id: 1,
+        name: 'Frutas',
+        actual: 0,
+        meta: 2
+    },
+    {
+        id: 2,
+        name: 'Cereales sin Grasa',
+        actual: 0,
+        meta: 8
+    },
+    {
+        id: 3,
+        name: 'Alimentos de Origen Animal',
+        actual: 0,
+        meta: 10
+    },
+    {
+        id: 4,
+        name: 'Leche',
+        actual: 0,
+        meta: 2
+    },
+    {
+        id: 5,
+        name: 'Aceite sin Proteína',
+        actual: 0,
+        meta: 9
+    },
+    {
+        id: 6,
+        name: 'Aceite con Proteína',
+        actual: 0,
+        meta: 2
+    },
+    {
+        id: 7,
+        name: 'Azúcar',
+        actual: 0,
+        meta: 2
+    },
+];
+
+export default FOOD_TYPES;
